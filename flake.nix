4ff6c1658d8da067c928971378b9907c2d0e17cf
Kovacsics Robert <kovirobi@gmail.com>
2022-12-26 21:07:44 +0000

    Initial commit, usbd-human-interface-device example
    Using template from usbd-human-interface-device examples.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            rustup
+            # cargo
+            rust-analyzer
+            flip-link
+            gcc-arm-embedded
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      });
+}
